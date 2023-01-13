d0dabc5edc2971d3cdff04e40829283aad5fded7
Hieu Mau <mauhieu12@gmail.com>
2023-01-13 17:11:48 +0700

    add info table

--- src/app/modules/feature/dashboard/dashboard.module.ts
+++ src/app/modules/feature/dashboard/dashboard.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 import {MatProgressBarModule} from "@angular/material/progress-bar";
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
@@ -17,7 +18,8 @@ import { TableCellProgressBackgroundComponent } from './table-cell-progess-backg
         CommonModule,
         DashboardRoutingModule,
         MatProgressBarModule,
-        MatButtonModule
+        MatButtonModule,
+        MatIconModule,
     ]
 })
 export class DashboardModule { }
